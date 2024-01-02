956ff64fc38a28c4e62cba22f56779166bb1ad2f
leekyuho <rbgh0114@naver.com>
2024-01-03 04:16:02 +0900

    chore: font family 추가 안했었음(#2)

--- src/styles/font.tsx
+++ src/styles/font.tsx
@@ -2,54 +2,63 @@ import styled from 'styled-components';
 //<Heading color={Red}> 이런식으로 사용
 //semi-bold : 600 regular : 400
 export const Heading = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 2rem;
   font-weight: 600;
   line-height: 100%;
 `;
 export const Subtitle = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 1.8rem;
   font-weight: 600;
   line-height: 135%;
 `;
 export const Body1 = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 1.6rem;
   font-weight: 600;
   line-height: 150%;
 `;
 export const Body2 = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 1.6rem;
   font-weight: 400;
   line-height: 150%;
 `;
 export const Body3 = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 1.4rem;
   font-weight: 400;
   line-height: 155%;
 `;
 export const Button1 = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 1.6rem;
   font-weight: 600;
   line-height: 125%;
 `;
 export const Button2 = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 1.4rem;
   font-weight: 400;
   line-height: 125%;
 `;
 export const Caption1 = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 1.2rem;
   font-weight: 600;
   line-height: 140%;
 `;
 export const Caption2 = styled.div`
+  font-family: Pretendard;
   color: ${(props) => props.color || '#000000'};
   font-size: 1.2rem;
   font-weight: 400;
