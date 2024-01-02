6f850ad44cb62b89cfbc440b5b32af54e631fa53
leekyuho <rbgh0114@naver.com>
2024-01-02 23:34:51 +0900

    feat: router에 navbar 구현, 페이지 구현 시 추가(#2)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,24 @@
 import { BuyerHome } from 'pages/Buyer/BuyerHome';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 const Router = () => {
+  const navigate = useNavigate();
   return (
     <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <button
+              onClick={() => {
+                navigate('/buyer');
+              }}
+            >
+              BuyerHome
+            </button>
+          </>
+        }
+      />
       <Route path="/buyer" element={<BuyerHome />} />
     </Routes>
   );
