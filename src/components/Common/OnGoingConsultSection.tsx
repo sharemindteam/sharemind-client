0a6a1a173f2129f71a14293a4718ef6cac86461e
inyoung <rmdnps10@gmail.com>
2024-01-04 02:16:03 +0900

    Feat: 판매자 홈페이지에서 섹션 단위로 컴포넌트 분리 #10

---
+++ src/components/Common/OnGoingConsultSection.tsx
@@ -0,0 +1,54 @@
+import { ContentTag } from 'pages/Seller/SellerHome';
+import React from 'react';
+import styled from 'styled-components';
+import { Black, Red } from 'styles/color';
+import { Body1, Heading } from 'styles/font';
+import OngoingCounsultBox from './OngoingCounsultBox';
+import { ReactComponent as RightArrow } from 'assets/icons/right-arrow.svg';
+function OnGoingConsultSection() {
+  return (
+    <>
+      <ContentTag>
+        <Heading color={Black}>진행중인 상담</Heading>
+        <Body1 color={Red} margin="0px auto 0px 0px">
+          5
+        </Body1>
+        <RightArrow />
+      </ContentTag>
+      <OngoingCounsultBoxList>
+        <OngoingCounsultBox
+          consultStatus="상담 중"
+          counselorName="연애상담마스터"
+          beforeMinutes="8분 전"
+          content="연애상담마스터님께 고민 내용을 남겨주세요. 연애상담마스터님이 24시간"
+          newMessageCounts="1"
+          counselorprofileStatus={1}
+        />{' '}
+        <OngoingCounsultBox
+          consultStatus="상담 중"
+          counselorName="연애상담마스터"
+          beforeMinutes="8분 전"
+          content="연애 상담마스터님께 고민 내용을 남겨주세요. 연애 상담마스터님이 어쩌구"
+          newMessageCounts="1"
+          counselorprofileStatus={2}
+        />{' '}
+        <OngoingCounsultBox
+          consultStatus="상담 중"
+          counselorName="연애상담마스터"
+          beforeMinutes="8분 전"
+          content="연애 상담마스터님께 고민 내용을 남겨주세요. 연애 상담마스터님이 어쩌구"
+          newMessageCounts="1"
+          counselorprofileStatus={3}
+        />
+      </OngoingCounsultBoxList>
+    </>
+  );
+}
+
+const OngoingCounsultBoxList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.8rem;
+`;
+export default OnGoingConsultSection;
--- src/pages/Seller/SellerHome.tsx
+++ src/pages/Seller/SellerHome.tsx
@@ -6,6 +6,8 @@ import { ReactComponent as RightArrow } from 'assets/icons/right-arrow.svg';
 import { Black, Red } from 'styles/color';
 import { Body1, Heading } from 'styles/font';
 import OngoingCounslutBox from 'components/Common/OngoingCounsultBox';
+import { IncomeManagementSection } from 'components/Common/IncomeManagementSection';
+import OnGoingConsultSection from 'components/Common/OnGoingConsultSection';
 export const SellerHome = () => {
   const navigate = useNavigate();
   return (
@@ -18,51 +20,12 @@ export const SellerHome = () => {
           }}
         />
         <TabA1 isBuyer={false} initState={1} />
-        <ContentTag>
-          <Heading color={Black}>진행중인 상담</Heading>
-          <Body1 color={Red} margin="0px auto 0px 0px">
-            5
-          </Body1>
-          <RightArrow />
-        </ContentTag>
-        <OngoingCounslutBoxList>
-          <OngoingCounslutBox
-            consultStatus="상담 중"
-            counselorName="연애상담마스터"
-            beforeMinutes="8분 전"
-            content="연애상담마스터님께 고민 내용을 남겨주세요. 연애상담마스터님이 24시간"
-            newMessageCounts="1"
-            counselorprofileStatus={1}
-          />{' '}
-          <OngoingCounslutBox
-            consultStatus="상담 중"
-            counselorName="연애상담마스터"
-            beforeMinutes="8분 전"
-            content="연애 상담마스터님께 고민 내용을 남겨주세요. 연애 상담마스터님이 어쩌구"
-            newMessageCounts="1"
-            counselorprofileStatus={2}
-          />{' '}
-          <OngoingCounslutBox
-            consultStatus="상담 중"
-            counselorName="연애상담마스터"
-            beforeMinutes="8분 전"
-            content="연애 상담마스터님께 고민 내용을 남겨주세요. 연애 상담마스터님이 어쩌구"
-            newMessageCounts="1"
-            counselorprofileStatus={3}
-          />
-        </OngoingCounslutBoxList>
-        <ContentTag>
-          <Heading color={Black} margin="0px auto 0px 0px">
-            수익 관리
-          </Heading>
-
-          <RightArrow />
-        </ContentTag>{' '}
+        <OnGoingConsultSection />
+        <IncomeManagementSection />
         <ContentTag>
           <Heading color={Black} margin="0px auto 0px 0px">
             상담 후기
           </Heading>
-
           <RightArrow />
         </ContentTag>
       </section>
@@ -70,16 +33,9 @@ export const SellerHome = () => {
   );
 };
 
-const ContentTag = styled.div`
+export const ContentTag = styled.div`
   display: flex;
   gap: 0.8rem;
   margin: 2.2rem 3.2rem 2.2rem 2rem;
   align-items: center;
-`;
-
-const OngoingCounslutBoxList = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.8rem;
 `;
