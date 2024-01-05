0a3440686701d23738f01481bd57263a62d9da6c
inyoung <rmdnps10@gmail.com>
2024-01-06 03:47:09 +0900

    Feat: 판매자 프로필  수정 페이지 UI 틀 구현 #11

--- src/components/Seller/SellerMyPageModifyProfile/ModifyProfileMainSection.tsx
+++ src/components/Seller/SellerMyPageModifyProfile/ModifyProfileMainSection.tsx
@@ -1,6 +1,6 @@
 import Input from 'components/Common/Input';
 import styled from 'styled-components';
-import { Grey3, Grey4, White } from 'styles/color';
+import { Grey1, Grey3, Grey4, Grey6, White } from 'styles/color';
 import { Body1, Caption2 } from 'styles/font';
 import { ReactComponent as CheckIcon } from 'assets/icons/icon-check.svg';
 export const ModifyProfileMainSection = () => {
@@ -49,8 +49,26 @@ export const ModifyProfileMainSection = () => {
         </div>
       </ModifyProfileBox>
       <ModifyProfileBox>
-        <div className="one-liner"></div>
-        <div className="experience"></div>
+        <div className="one-liner">
+          <ProfileInformTag>한줄 소개</ProfileInformTag>
+          <Input width="100%" height="4.8rem" />
+          <ConditionMessage>
+            <Caption2 color={Grey4}>
+              최대 50자 / 한글, 영문, 숫자 가능 (특수문자 불가)
+            </Caption2>
+            <CheckIcon />
+          </ConditionMessage>
+        </div>
+        <div className="experience">
+          <ProfileInformTag>경험 소개</ProfileInformTag>
+          <ExperienceTextArea />
+          <ConditionMessage>
+            <Caption2 color={Grey4}>
+              최대 20,000자 / 한글, 영문, 숫자 가능 (특수문자 불가)
+            </Caption2>
+            <CheckIcon />
+          </ConditionMessage>
+        </div>
       </ModifyProfileBox>
     </ModifyProfileMainSectionWrapper>
   );
@@ -94,4 +112,22 @@ const ConditionMessage = styled.div`
 
 const PriceInformTag = styled(Body1)`
   margin-right: auto;
+`;
+
+const ExperienceTextArea = styled.textarea`
+  height: 44rem;
+  border: none;
+  resize: none;
+  background-color: ${Grey6};
+  box-sizing: border-box;
+  padding: 1.6rem;
+  width: 100%;
+  color: ${Grey1};
+  border-radius: 1.2rem;
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 150%;
+  &:focus {
+    outline: none;
+  }
 `;
---
+++ src/utils/profileDummy.ts
@@ -0,0 +1,9 @@
+export const profileDummyData = {
+    nickName : "연애상담마스터",
+    category: "이별/재회, 권태기, 남자심리",
+    style:"조언",
+    type:"",
+    time:"",
+    letterPrice: "10000원",
+    chatPrice:"40000"
+}
