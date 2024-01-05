9ee44d6ca0b968cf1c123a9d17ea545b70ba925d
leekyuho <rbgh0114@naver.com>
2024-01-06 04:15:14 +0900

    feat: 상담카드 unread 추가 (#1)

--- src/components/Buyer/Common/ConsultCard.tsx
+++ src/components/Buyer/Common/ConsultCard.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Black, Grey1, Grey2, Grey6 } from 'styles/color';
+import { Black, Green, Grey1, Grey2, Grey6, White } from 'styles/color';
 import { Body1, Caption2 } from 'styles/font';
 
 import { Characters } from 'utils/Characters';
@@ -10,12 +10,14 @@ interface ConsultCardProps {
   consultState: ConsultState;
   time: string;
   content: string;
+  unread: number;
 }
 export const ConsultCard = ({
   name,
   consultState,
   time,
   content,
+  unread,
 }: ConsultCardProps) => {
   return (
     <Wrapper>
@@ -30,6 +32,11 @@ export const ConsultCard = ({
               <Body1 color={Black}>{name}</Body1>
               <Caption2 color={Grey2}>•</Caption2>
               <Caption2 color={Grey2}>{time}</Caption2>
+              {unread > 0 ? (
+                <Unread>
+                  <Caption2 color={White}>{unread}</Caption2>
+                </Unread>
+              ) : null}
             </div>
             <CardText color={Grey1}>{content}</CardText>
           </div>
@@ -48,6 +55,17 @@ const Wrapper = styled.div`
 const ConsultContent = styled.div`
   padding: 1.6rem;
 `;
+const Unread = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.4rem;
+  background-color: ${Green};
+  width: 1.9rem;
+  height: 1.9rem;
+  right: 0;
+`;
 const ConsultStateBox = styled.div`
   display: flex;
   gap: 1.6rem;
@@ -62,6 +80,7 @@ const ConsultStateBox = styled.div`
     align-items: center;
     margin-top: 0.2rem;
     gap: 0.8rem;
+    position: relative;
   }
   .col2 {
     display: flex;
