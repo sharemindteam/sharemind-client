afee1d391d83e492d8e4e80b83875b3dccc0877f
inyoung <rmdnps10@gmail.com>
2024-01-06 06:21:52 +0900

    Fix: 주소에 따라 AppContainer 색상 변경되지 않는 오류 해결

--- src/components/Common/AppContainer.tsx
+++ src/components/Common/AppContainer.tsx
@@ -1,4 +1,5 @@
 import { useViewResize } from 'hooks/useViewResize';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import { Grey6, White } from 'styles/color';
@@ -8,11 +9,23 @@ interface AppContainerProps {
 
 export const AppContainer = ({ children }: AppContainerProps) => {
   useViewResize();
-  const { pathname } = useLocation();
-  console.log(pathname);
-  return <StyledApp currentPath={pathname}>{children}</StyledApp>;
+  var { pathname } = useLocation();
+  const [isGray, setIsGray] = useState(false);
+  useEffect(() => {
+    if (
+      pathname ===
+      ('/seller/mypage/viewProfile' ||
+        '/seller/mypage' ||
+        '/buyer/mypage' ||
+        '/seller/mypage/modifyProfile')
+    ) {
+      setIsGray(true);
+    }
+  }, [pathname]);
+
+  return <StyledApp isGray={isGray}>{children}</StyledApp>;
 };
-const StyledApp = styled.div<{ currentPath: string }>`
+const StyledApp = styled.div<{ isGray: boolean }>`
   height: calc(var(--vh, 1vh) * 100);
   @media (max-width: 767px) {
     width: 100vw;
@@ -20,11 +33,6 @@ const StyledApp = styled.div<{ currentPath: string }>`
   @media (min-width: 768px) {
     width: 37.5rem;
   }
-  background-color: ${(props) =>
-    props.currentPath === '/seller/mypage' ||
-    '/buyer/mypage' ||
-    '/seller/mypage/viewProfile'
-      ? Grey6
-      : White};
+  background-color: ${(props) => (props.isGray ? Grey6 : White)};
   overflow-y: scroll;
 `;
