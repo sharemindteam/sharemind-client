fb9816c2540f920b2b95b7976f56f8b225cca0d8
inyoung <rmdnps10@gmail.com>
2024-01-06 22:53:42 +0900

    Feat: Merge before commit

--- src/Router.tsx
+++ src/Router.tsx
@@ -55,6 +55,11 @@ const Router = () => {
       {/* 판매자 : 홈 */}
       <Route path="/seller" element={<SellerHome />} />
       {/* 판매자 : 상담 */}
+
+      {/* 판매자 : 편지 */}
+      <Route path="/seller/letter/:consultid" />
+
+
       <Route path="/seller/consult" element={<SellerConsult />} />
       {/* 판매자 : 프로필 정보 */}
       <Route path="/seller/mypage" element={<SellerMypage />} />
