b054c8d5e93b4da0bf44edbef816617d062bc7a5
inyoung <rmdnps10@gmail.com>
2024-01-24 02:55:44 +0900

    Feat: 상담 시간 선택 모달 view 구현 #73

--- src/components/Seller/SellerMyPageModifyProfile/TimeSelectModal.tsx
+++ src/components/Seller/SellerMyPageModifyProfile/TimeSelectModal.tsx
@@ -1,3 +1,4 @@
+import { Space } from 'components/Common/Space';
 import React, { SetStateAction } from 'react';
 import { useRecoilState } from 'recoil';
 import styled, { keyframes } from 'styled-components';
@@ -5,7 +6,7 @@ import { Green, Grey3, Grey4, Grey6 } from 'styles/color';
 import { Body1, Button2, Caption2 } from 'styles/font';
 import { isTimeModalOpenState } from 'utils/atom';
 // 0부터 24까지 배열 생성
-const hours = Array.from({ length: 25 }, (_, index) => index); 
+const hourList = Array.from({ length: 25 }, (_, index) => index);
 
 interface TimeSelectModalProps {}
 function TimeSelectModal({}: TimeSelectModalProps) {
@@ -26,9 +27,39 @@ function TimeSelectModal({}: TimeSelectModalProps) {
           시작시간과 종료시간을 1시간 이상 간격으로 설정해주세요
         </Caption2>
       </div>
+      <Space height="1.6rem" />
+      <HourButtonList>
+        {hourList.map((hour) => (
+          <HourButton key={hour}>
+            <Button2>{hour < 10 ? `0${hour}` : hour}시</Button2>
+          </HourButton>
+        ))}
+        <Caption2 color={Grey3}>
+          24시 이후에 종료하고 싶은 경우, 다음 요일의 <br />
+          항목에서 시작과 종료를 별도로 설정해주세요
+        </Caption2>
+      </HourButtonList>
     </Wrapper>
   );
 }
+const HourButtonList = styled.div`
+  display: flex;
+  gap: 2.4rem;
+  margin: 0 3.15rem;
+  flex-wrap: wrap;
+`;
+
+const HourButton = styled.div`
+  display: flex;
+  cursor: pointer;
+  background-color: white;
+  width: 6rem;
+  height: 3.4rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1.2rem;
+`;
+
 const slideIn = keyframes`
   from{
     transform : translateY(100%);
@@ -66,7 +97,7 @@ const Wrapper = styled.div<{ visible: boolean }>`
   z-index: 2002;
   animation: ${({ visible }) => (visible ? slideIn : slideOut)} 0.3s ease-in-out;
   .bar-wrapper {
-    height: 4.5rem;
+    height: 2.7rem;
     display: flex;
     justify-content: center;
   }
