b01ae17c6de9ec2881fcddd2e1f6cec09ceb7435
leekyuho <rbgh0114@naver.com>
2024-01-31 23:13:53 +0900

    feat: 로그인 페이지 비밀번호 pwInput 으로 변경 (#46)

--- src/pages/Buyer/BuyerLogin.tsx
+++ src/pages/Buyer/BuyerLogin.tsx
@@ -9,6 +9,7 @@ import { useInput } from 'hooks/useInput';
 import { BackDrop } from 'components/Common/BackDrop';
 import { LoginModal } from 'components/Buyer/BuyerLogin/LoginModal';
 import { useState } from 'react';
+import PwInput from 'components/Buyer/Common/PwInput';
 export const BuyerLogin = () => {
   const emailInput = useInput('');
   const pwInput = useInput('');
@@ -68,10 +69,12 @@ export const BuyerLogin = () => {
           </div>
           <div className="input-wrapper">
             <Body1 color={Grey3}>비밀번호</Body1>
-            <LoginInput
-              type="password"
+            <PwInput
+              width="33.5rem"
+              height="4.8rem"
               value={pwInput.value}
               onChange={pwInput.onChange}
+              textIndent="0.8rem"
             />
           </div>
           <div className="submit-option">
@@ -177,7 +180,7 @@ const LoginInput = styled.input<{
   font-weight: 600;
   line-height: 150%;
   color: ${Grey1};
-  text-indent: 0.5rem;
+  text-indent: 0.8rem;
   &::placeholder {
   }
   &:focus {
