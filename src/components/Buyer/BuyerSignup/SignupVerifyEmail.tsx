03457681b35e115c4875bb996ba70c442ef1d22d
leekyuho <rbgh0114@naver.com>
2024-02-16 23:29:46 +0900

    feat: 인증번호 전송 체크 아이콘 삭제 (#46)

--- src/components/Buyer/BuyerSignup/SignupVerifyEmail.tsx
+++ src/components/Buyer/BuyerSignup/SignupVerifyEmail.tsx
@@ -209,7 +209,6 @@ export const SignupVerifyEmail = ({
             ) : (
               <>
                 <Caption2 color={errorMessageColor}>{errorMessage}</Caption2>
-                <Check />
               </>
             )}
           </div>
