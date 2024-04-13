c0c4640bc0c41538313b7b7856d5597649456998
kyuhho <rbgh0114@naver.com>
2024-04-13 18:55:49 +0900

    refactor: apply `BuyerChatSection` component (#215)

--- src/pages/Buyer/BuyerChat.tsx
+++ src/pages/Buyer/BuyerChat.tsx
@@ -1,43 +1,19 @@
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import {
-  Body2,
-  Body3,
-  Button1,
-  Button2,
-  Caption1,
-  Caption2,
-  Heading,
-} from 'styles/font';
+import { Heading } from 'styles/font';
 import { ChatCounselorInfo, ChatMessage } from 'utils/type';
 import styled from 'styled-components';
-import {
-  Black,
-  Green,
-  Grey1,
-  Grey3,
-  Grey4,
-  Grey6,
-  LightGreenChat,
-  White,
-} from 'styles/color';
+import { Grey1, Grey6, White } from 'styles/color';
 import { BackIcon } from 'components/Buyer/Common/Header';
 import { useNavigate, useParams } from 'react-router-dom';
-import { formattedMessage } from 'utils/formattedMessage';
 import { getChatMessagesCustomers, getCounselorsChats } from 'api/get';
 import useIntersectionObserver from 'hooks/useIntersectionObserver';
 import { Space } from 'components/Common/Space';
-import { Button } from 'components/Common/Button';
-import {
-  calculateTimeAfterFiveMinutes,
-  convertAMPMToString,
-  convertAMPMToStringYear,
-  convertMessageTime,
-} from 'utils/convertDate';
+import { calculateTimeAfterFiveMinutes } from 'utils/convertDate';
 
-import { ChatCounselorInfoBox } from 'components/Buyer/BuyerChat/ChatCounselorInfoBox';
 import { useStompContext } from 'contexts/StompContext';
 import useChatRequestTime from 'hooks/Chat/useChatRequestTime';
 import BuyerChatFooter from 'components/Buyer/BuyerChat/BuyerChatFooter';
+import BuyerChatSection from 'components/Buyer/BuyerChat/BuyerChatSection';
 
 export const BuyerChat = () => {
   const navigate = useNavigate();
@@ -365,6 +341,7 @@ export const BuyerChat = () => {
       preventRef.current = true;
     }
   };
+
   //현재 대상 및 option을 props로 전달
   const { setTarget } = useIntersectionObserver({
     root: null,
@@ -408,6 +385,7 @@ export const BuyerChat = () => {
       setIsTyping(false);
     }
   }, [input]);
+
   // // messages 새로 업데이트 됐을 때 11 index에 해당하는 message top으로
   useLayoutEffect(() => {
     //scrollIntoView 완료하기까지 관측 X
@@ -419,8 +397,6 @@ export const BuyerChat = () => {
         block: 'start', // 페이지 하단으로 스크롤하도록 지정합니다.
       });
     }
-    // console.log('스크롤 완료');
-    //scrollIntoView 완료 후 다시 관측가능
     preventScrollRef.current = true;
   }, [messages]);
 
@@ -435,211 +411,19 @@ export const BuyerChat = () => {
         <Heading color={Grey1}>{counselorInfo?.nickname}</Heading>
       </HeaderWrapper>
       <Space width="100%" height="5.2rem" />
-      <SectionWrapper inputHeight={sectionPaddingRef.current}>
-        <div className="counselor-info-container">
-          {isLastElem && counselorInfo !== null && (
-            <ChatCounselorInfoBox info={counselorInfo} />
-          )}
-        </div>
-        {!isLastElem ? (
-          <div
-            ref={setTarget}
-            style={{
-              width: '100%',
-              backgroundColor: 'green',
-            }}
-          ></div>
-        ) : (
-          <div
-            style={{
-              width: '100%',
-              backgroundColor: 'pink',
-            }}
-          ></div>
-        )}
-        {messages.map((value, index) => {
-          let isLastIndex = index === messages.length - 1;
-          //iscustomer가 true인거 customer message, 시작
-          //finish는 isCustomer true이지만 보이는건 상대 채팅임
-          if (value.isCustomer && value.chatMessageStatus !== 'FINISH') {
-            let isTimestampCustomer = true;
-            const length = messages.length;
-            if (length !== 0 && index !== length - 1) {
-              //다음메세지와 시간이 같으면 false
-              if (
-                messages[index + 1].chatMessageStatus === 'MESSAGE' &&
-                messages[index + 1].isCustomer &&
-                messages[index + 1].sendTime === value.sendTime
-              )
-                isTimestampCustomer = false;
-            }
-
-            return (
-              <div
-                key={value.messageId}
-                className="my-box-container"
-                ref={
-                  isLastIndex
-                    ? lastRef
-                    : index === topMsgIndexRef.current
-                    ? topRef
-                    : null
-                }
-              >
-                {value.chatMessageStatus === 'MESSAGE' && (
-                  <>
-                    {isTimestampCustomer ? (
-                      <Caption2 color={Grey3} margin="0 0.8rem 0 0">
-                        {convertMessageTime(value.sendTime)}
-                      </Caption2>
-                    ) : null}
-                    <CustomerChatBox>
-                      <Body2 color={Grey1}>
-                        {formattedMessage(value.content)}
-                      </Body2>
-                    </CustomerChatBox>
-                  </>
-                )}
-                {value.chatMessageStatus === 'START' && (
-                  <AlertCustomerChatBox>
-                    <Caption1 color={Grey3}>
-                      {value.content.split('\n')[0]}
-                    </Caption1>
-                    <Caption2 color={Grey4}>
-                      {convertAMPMToStringYear(value.content.split('\n')[1])}
-                    </Caption2>
-                  </AlertCustomerChatBox>
-                )}
-              </div>
-            );
-          } else if (
-            // isCustomer false 거나, FINISH(true인데 상대편에서 온걸로 처리)
-            value.isCustomer === false ||
-            value.chatMessageStatus === 'FINISH'
-          ) {
-            let isTimestampCounselor = true;
-            const length = messages.length;
-            if (length !== 0 && index !== length - 1) {
-              //다음메세지와 시간이 같으면 false
-              if (
-                messages[index + 1].chatMessageStatus === 'MESSAGE' &&
-                !messages[index + 1].isCustomer &&
-                messages[index + 1].sendTime === value.sendTime
-              )
-                isTimestampCounselor = false;
-            }
-            return (
-              <div
-                key={value.messageId}
-                className="opponent-box-container"
-                ref={
-                  isLastIndex
-                    ? lastRef
-                    : index === topMsgIndexRef.current
-                    ? topRef
-                    : null
-                }
-              >
-                {value.chatMessageStatus === 'MESSAGE' && (
-                  <>
-                    <CounselorChatBox>
-                      <Body2 color={Grey1}>
-                        {formattedMessage(value.content)}
-                      </Body2>
-                    </CounselorChatBox>
-                    {isTimestampCounselor ? (
-                      <Caption2 color={Grey3} margin="0 0 0 0.8rem">
-                        {convertMessageTime(value.sendTime)}
-                      </Caption2>
-                    ) : null}
-                  </>
-                )}
-                {value.chatMessageStatus === 'SEND_REQUEST' && (
-                  <CounselorStartRequestBox>
-                    <div style={{ paddingBottom: '1.6rem' }}>
-                      <Body3 color={Grey1}>
-                        {formattedMessage(value.content)}
-                      </Body3>
-                      <Body3 color={Grey3}>
-                        * 상담 시작하기를 누르시면 상담이 시작되어요. 상담
-                        시간은 30분입니다.
-                      </Body3>
-                    </div>
-                    <StartButton onClick={sendChatStartResponse}>
-                      <Button1 color={White}>상담 시작하기</Button1>
-                      <Button2 color={White}>{time}</Button2>
-                    </StartButton>
-                  </CounselorStartRequestBox>
-                )}
-
-                {value.chatMessageStatus === 'FINISH' && (
-                  <EndChatBox>
-                    <div style={{ paddingBottom: '1.6rem' }}>
-                      <Body3 color={Black}>
-                        {value.counselorNickname + value.content}
-                      </Body3>
-                    </div>
-                    <Button
-                      text="상담 후기 남기기"
-                      width="100%"
-                      height="5.2rem"
-                      onClick={() => {
-                        navigate('/reviewManage');
-                      }}
-                    />
-                  </EndChatBox>
-                )}
-              </div>
-            );
-          } else if (value.isCustomer === null) {
-            return (
-              <div
-                key={value.messageId}
-                className="opponent-box-container"
-                ref={
-                  isLastIndex
-                    ? lastRef
-                    : index === topMsgIndexRef.current
-                    ? topRef
-                    : null
-                }
-              >
-                {value.chatMessageStatus === 'FIVE_MINUTE_LEFT' && (
-                  <AlertCounselorChatBox>
-                    <Caption1 color={Grey3}>
-                      {value.content.split('\n')[0]}
-                    </Caption1>
-                    <Caption2 color={Grey4}>
-                      {convertAMPMToString(value.content.split('\n')[1])} 종료
-                    </Caption2>
-                  </AlertCounselorChatBox>
-                )}
-                {value.chatMessageStatus === 'TIME_OVER' && (
-                  <EndChatBox>
-                    <div style={{ paddingBottom: '1.6rem' }}>
-                      <Body3 color={Grey1}>
-                        {value.content.split('\n')[0]}
-                      </Body3>
-                      <Body3 color={Grey1}>
-                        {value.content.split('\n')[1]}
-                      </Body3>
-                      <Body3 color={Grey3}>
-                        {value.content.split('\n')[2]}
-                      </Body3>
-                    </div>
-                    <Button
-                      text="상담 종료하기"
-                      width="100%"
-                      height="5.2rem"
-                      onClick={sendChatFinishRequest}
-                    />
-                  </EndChatBox>
-                )}
-              </div>
-            );
-          }
-        })}
-      </SectionWrapper>
+      <BuyerChatSection
+        messages={messages}
+        time={time}
+        isLastElem={isLastElem}
+        counselorInfo={counselorInfo}
+        lastRef={lastRef}
+        topRef={topRef}
+        topMsgIndexRef={topMsgIndexRef}
+        sectionPaddingRef={sectionPaddingRef}
+        setTarget={setTarget}
+        sendChatStartResponse={sendChatStartResponse}
+        sendChatFinishRequest={sendChatFinishRequest}
+      />
       <BuyerChatFooter
         input={input}
         isTyping={isTyping}
@@ -676,90 +460,4 @@ const HeaderWrapper = styled.div<{ border?: boolean }>`
   }
   top: 0;
   z-index: 999;
-`;
-
-const SectionWrapper = styled.section<{ inputHeight: number }>`
-  display: flex;
-  flex-direction: column;
-  padding-bottom: ${(props) => `${props.inputHeight + 4.3}rem`};
-  max-height: calc(
-    100% - 5.2rem - ${(props) => `${props.inputHeight + 4.3}rem`}
-  );
-  overflow-y: auto;
-  .counselor-info-container {
-    display: flex;
-    width: 100%;
-    justify-content: center;
-  }
-  .my-box-container {
-    display: flex;
-    justify-content: flex-end;
-    align-items: end;
-    padding: 0.4rem 2rem 0.4rem 0;
-  }
-  .opponent-box-container {
-    display: flex;
-    justify-content: flex-start;
-    align-items: end;
-    padding: 0.4rem 0 0.4rem 2rem;
-  }
-`;
-
-const CustomerChatBox = styled.div`
-  background-color: ${LightGreenChat};
-  border-radius: 1rem 0 1rem 1rem;
-  padding: 1.2rem;
-  box-sizing: border-box;
-  max-width: 27.5rem;
-  word-wrap: break-word;
-`;
-const CounselorChatBox = styled.div`
-  background-color: ${White};
-  border-radius: 0 1rem 1rem 1rem;
-  padding: 1.2rem;
-  box-sizing: border-box;
-  max-width: 27.5rem;
-  word-wrap: break-word;
-`;
-const CounselorStartRequestBox = styled.div`
-  background-color: ${White};
-  border-radius: 0 1rem 1rem 1rem;
-  padding: 1.6rem;
-  box-sizing: border-box;
-  max-width: 23.9rem;
-`;
-const AlertCustomerChatBox = styled.div`
-  width: 100%;
-  padding: 0.4rem 0rem 0.4rem 2rem;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-//opponent box에서 padding 2rem 줘서 align을 위해 padding 추가
-const AlertCounselorChatBox = styled.div`
-  width: 100%;
-  padding: 0.4rem 2rem 0.4rem 0;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-const EndChatBox = styled.div`
-  background-color: ${White};
-  border-radius: 0 1rem 1rem 1rem;
-  padding: 1.6rem;
-  box-sizing: border-box;
-  max-width: 23.9rem;
-`;
-const StartButton = styled.button`
-  background-color: ${Green};
-  width: 100%;
-  height: 5.2rem;
-  border-radius: 0.8rem;
-  cursor: pointer;
-  display: flex;
-  gap: 0.8rem;
-  justify-content: center;
-  align-items: center;
 `;
