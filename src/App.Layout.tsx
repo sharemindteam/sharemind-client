deab1fe40fa7c8a69f72482f42e8db42011214a1
kyuhho <rbgh0114@naver.com>
2024-06-01 18:11:43 +0900

    feat: add margin ratio (#290)

--- src/App.Layout.tsx
+++ src/App.Layout.tsx
@@ -16,6 +16,7 @@ const LAYOUT_STYLES = {
   backgroundRepeat: 'no-repeat',
   width: '100vw',
   height: '100vh',
+  display: 'flex',
 };
 
 //
@@ -23,7 +24,13 @@ const LAYOUT_STYLES = {
 //
 
 const AppLayout = ({ children }: AppLayoutProps) => {
-  return <div style={LAYOUT_STYLES}>{children}</div>;
+  return (
+    <div style={LAYOUT_STYLES}>
+      <div style={{ flex: 5 }} />
+      <div style={{ flex: 1 }}>{children}</div>
+      <div style={{ flex: 1 }} />
+    </div>
+  );
 };
 
 export default AppLayout;
