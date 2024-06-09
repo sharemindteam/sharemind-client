485454026c09ce454def380d160546db34ceb652
kyuhho <rbgh0114@naver.com>
2024-06-09 22:39:57 +0900

    feat: apply infinite query with `useInfiniteQuery` (#294)

--- src/pages/Buyer/BuyerOpenConsultRecents.tsx
+++ src/pages/Buyer/BuyerOpenConsultRecents.tsx
@@ -1,5 +1,18 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getPostsCustomersPublic } from 'api/get';
+import OpenConsultCard from 'components/Buyer/BuyerOpenConsult/OpenConsultCard';
+import { getPostsCustomersPublicResponse } from 'components/Buyer/BuyerOpenConsult/OpenConsultList';
 import AppHeader from 'components/Common/AppHeader';
+import { Flex } from 'components/Common/Flex';
+import useInfiniteObserver from 'hooks/useInfiniteObserver';
+import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+//
+//
+//
+
+const RECENT_LIST_PER_PAGE = 4;
 
 //
 //
@@ -7,6 +20,51 @@ import { useNavigate } from 'react-router-dom';
 
 const BuyerOpenConsultRecents = () => {
   const navigate = useNavigate();
+
+  const {
+    data: recentConsults,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    isLoadingError,
+  } = useInfiniteQuery<getPostsCustomersPublicResponse[]>({
+    queryKey: ['infiniteGetPostsCustomersPublic'],
+    queryFn: async ({ pageParam }) =>
+      await getPostsCustomersPublic(pageParam).then((res) => {
+        return res?.data;
+      }),
+    initialPageParam: {
+      postId: 0,
+      finishedAt: new Date().toISOString().slice(0, 19),
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.length < RECENT_LIST_PER_PAGE) {
+        return undefined;
+      }
+
+      const lastItem = lastPage[lastPage.length - 1];
+
+      return {
+        postId: lastItem.postId,
+        finishedAt: lastItem.finishedAt,
+      };
+    },
+  });
+
+  const recentConsultList = useMemo(
+    () => recentConsults?.pages.flatMap((channel) => channel) ?? [],
+    [recentConsults],
+  );
+
+  const { observerElem } = useInfiniteObserver({
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    isLoadingError,
+  });
+
   //
   //
   //
@@ -18,6 +76,23 @@ const BuyerOpenConsultRecents = () => {
           navigate('/open-consult');
         }}
       />
+      <Flex direction="column" gap="1.2rem" padding="1.6rem 2rem 7.2rem 2rem">
+        {recentConsultList?.map((consult) => (
+          <OpenConsultCard
+            key={consult.postId}
+            title={consult.title}
+            content={consult.content}
+            totalLike={consult.totalLike}
+            totalScrap={consult.totalScrap}
+            totalComment={consult.totalComment}
+            updatedAt={consult.updatedAt}
+            onClick={() => {
+              navigate(`/open-consult/${consult.postId}`);
+            }}
+          />
+        ))}
+        <div ref={observerElem} />
+      </Flex>
     </>
   );
 };
