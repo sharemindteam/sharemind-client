bcd85038d8398735d89c29f5aed05dfd4b9e45d0
inyoung <rmdnps10@gmail.com>
2024-06-19 16:43:20 +0900

    Chore: Replace header logo with one new SVG file #307

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
-import { ReactComponent as LogoBuyer } from 'assets/icons/logo-buyer.svg';
-import { ReactComponent as LogoSeller } from 'assets/icons/logo-seller.svg';
-import { ReactComponent as LogoText } from 'assets/icons/logo-text.svg';
+import { ReactComponent as LogoImg } from 'assets/icons/logo.svg';
 import { ReactComponent as Search } from 'assets/icons/search.svg';
 import { useNavigate } from 'react-router-dom';
 //
@@ -9,21 +7,17 @@ import { useNavigate } from 'react-router-dom';
 //
 interface HeaderProps {
   isBuyer: boolean;
-  onClick?: React.MouseEventHandler<HTMLDivElement>;
+  onClick?: React.MouseEventHandler<SVGElement>;
 }
 //
 //
 //
 export const Header = ({ isBuyer, onClick }: HeaderProps) => {
   const navigate = useNavigate();
-
   if (isBuyer === true) {
     return (
       <Wrapper>
-        <Logo onClick={onClick}>
-          <LogoBuyer />
-          <LogoText />
-        </Logo>
+        <Logo onClick={onClick}></Logo>
         <StyledSearch
           onClick={() => {
             if (isBuyer) {
@@ -38,10 +32,7 @@ export const Header = ({ isBuyer, onClick }: HeaderProps) => {
   } else {
     return (
       <Wrapper>
-        <Logo onClick={onClick}>
-          <LogoSeller />
-          <LogoText />
-        </Logo>
+        <Logo onClick={onClick} />
       </Wrapper>
     );
   }
@@ -56,10 +47,9 @@ const Wrapper = styled.header`
   top: 0;
   z-index: 999;
 `;
-const Logo = styled.div`
+const Logo = styled(LogoImg)`
   margin-left: 2.1rem;
   margin-top: 1.4rem;
-  display: flex;
   cursor: pointer;
 `;
 const StyledSearch = styled(Search)`
