06d3d84f6382bc03de4b8b8a028413dbb7af6a22
kyuhho <rbgh0114@naver.com>
2024-07-03 15:22:13 +0900

    feat: add beta logo (#327)

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -2,22 +2,37 @@ import styled from 'styled-components';
 import { ReactComponent as LogoImg } from 'assets/icons/logo.svg';
 import { ReactComponent as Search } from 'assets/icons/search.svg';
 import { useNavigate } from 'react-router-dom';
+
+import betaTagImageUrl from 'assets/logo/beta_logo.png';
+import { Flex } from './Flex';
+
 //
 //
 //
+
 interface HeaderProps {
   isBuyer: boolean;
   onClick?: React.MouseEventHandler<SVGElement>;
 }
+
 //
 //
 //
+
 export const Header = ({ isBuyer, onClick }: HeaderProps) => {
   const navigate = useNavigate();
+
+  //
+  //
+  //
+
   if (isBuyer === true) {
     return (
       <Wrapper>
-        <Logo onClick={onClick}></Logo>
+        <Flex gap="0.8rem" style={{ marginLeft: '2rem' }}>
+          <Logo onClick={onClick} />
+          <BetaTagImage src={betaTagImageUrl} />
+        </Flex>
         <StyledSearch
           onClick={() => {
             if (isBuyer) {
@@ -37,6 +52,7 @@ export const Header = ({ isBuyer, onClick }: HeaderProps) => {
     );
   }
 };
+
 const Wrapper = styled.header`
   width: 100%;
   height: 6rem;
@@ -47,13 +63,18 @@ const Wrapper = styled.header`
   top: 0;
   z-index: 999;
 `;
+
 const Logo = styled(LogoImg)`
-  margin-left: 2.1rem;
-  margin-top: 1.4rem;
   cursor: pointer;
 `;
+
 const StyledSearch = styled(Search)`
   margin-top: 1.6rem;
   margin-right: 2.17rem;
   cursor: pointer;
+`;
+
+const BetaTagImage = styled.img`
+  width: 4.6rem;
+  height: 2.2rem;
 `;
