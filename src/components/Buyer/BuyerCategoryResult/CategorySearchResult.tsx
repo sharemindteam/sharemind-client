22c2ad195b544be9623bd748f6107297ad968dff
kyuhho <rbgh0114@naver.com>
2024-07-09 20:33:43 +0900

    feat: apply `CounselorCard`on category search (#339)

--- src/components/Buyer/BuyerCategoryResult/CategorySearchResult.tsx
+++ src/components/Buyer/BuyerCategoryResult/CategorySearchResult.tsx
@@ -4,6 +4,7 @@ import { SearchResultData } from 'utils/type';
 import { AppendCategoryType } from 'utils/AppendCategoryType';
 import { consultStyleToCharNum } from 'utils/convertStringToCharNum';
 import { Flex } from 'components/Common/Flex';
+import CounselorCard from 'components/Common/CounselorCard';
 interface CategorySearchResultsProps {
   searchData: SearchResultData[];
 }
@@ -16,7 +17,7 @@ export const CategorySearchResults = ({
     <Flex direction="column" gap="0.8rem" style={{ padding: '0 2rem' }}>
       {searchData.map((value) => {
         return (
-          <ReadyConsultCard
+          <CounselorCard
             key={value.counselorId}
             counselorId={value.counselorId}
             tagList={AppendCategoryType(
@@ -24,16 +25,12 @@ export const CategorySearchResults = ({
               value.consultStyle,
             )}
             consultStyle={consultStyleToCharNum(value.consultStyle)}
-            consultTimes={value.consultTimes}
             introduction={value.introduction}
             nickname={value.nickname}
             level={value.level}
             isWishList={value.isWishList}
             rating={value.ratingAverage}
             totalReview={value.totalReview}
-            consultType={value.consultTypes}
-            letterPrice={value.consultCosts.편지}
-            chattingPrice={value.consultCosts.채팅}
             totalConsult={value.totalConsult}
           />
         );
