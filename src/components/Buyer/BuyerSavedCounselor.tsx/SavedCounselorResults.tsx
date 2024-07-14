47f41ae11dc6927f74262f493dd3fb4013c30567
kyuhho <rbgh0114@naver.com>
2024-07-14 17:10:59 +0900

    feat: apply infinite query (#350)

--- src/components/Buyer/BuyerSavedCounselor.tsx/SavedCounselorResults.tsx
+++ src/components/Buyer/BuyerSavedCounselor.tsx/SavedCounselorResults.tsx
@@ -3,25 +3,103 @@ import { WishlistDataType } from 'utils/type';
 import { AppendCategoryType } from 'utils/AppendCategoryType';
 import { consultStyleToCharNum } from 'utils/convertStringToCharNum';
 import CounselorCard from 'components/Common/CounselorCard';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { postWishLists } from 'api/post';
+import { useMemo } from 'react';
+import useInfiniteObserver from 'hooks/useInfiniteObserver';
+import { Space } from 'components/Common/Space';
+import { LoadingSpinner } from 'utils/LoadingSpinner';
+import EmptySection from 'components/Common/EmptySection';
 
 //
 //
 //
 
-interface SavedCounselorResultsProps {
-  wishlistData: WishlistDataType[];
-}
+const INFINITE_POST_WISH_LISTS_QUERY_KEY = 'infinitePostWishLists';
+
+const WISH_LIST_PER_PAGE = 4;
 
 //
 //
 //
 
-export const SavedCounselorResults = ({
-  wishlistData,
-}: SavedCounselorResultsProps) => {
+export const SavedCounselorResults = () => {
+  //
+  //
+  //
+  const {
+    data: wishlistData,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    isLoadingError,
+  } = useInfiniteQuery<WishlistDataType[]>({
+    queryKey: [INFINITE_POST_WISH_LISTS_QUERY_KEY],
+    queryFn: async ({ pageParam }) =>
+      await postWishLists(pageParam).then((res: any) => res.data),
+    initialPageParam: {
+      wishlistId: 0,
+      updatedAt: '',
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.length < WISH_LIST_PER_PAGE) {
+        return undefined;
+      }
+
+      const lastItem = lastPage[lastPage.length - 1];
+
+      return {
+        wishlistId: lastItem.wishlistId,
+        updatedAt: lastItem.updatedAt,
+      };
+    },
+  });
+
+  const wishlists = useMemo(
+    () => wishlistData?.pages.flatMap((wishlist) => wishlist) ?? [],
+    [wishlistData],
+  );
+
+  const { observerElem } = useInfiniteObserver({
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    isLoadingError,
+  });
+
+  //
+  //
+  //
+
+  if (isFetching && !isFetchingNextPage) {
+    return (
+      <div
+        style={{
+          height: '70vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (wishlists.length === 0) {
+    return (
+      <EmptySection
+        title="아직 찜한 마인더가 없어요."
+        subtitle={`관심 있는 마인더를 찜하고\n더욱 편리하게 상담하세요.`}
+      />
+    );
+  }
+
   return (
     <Wrapper>
-      {wishlistData.map((value) => {
+      {wishlists.map((value) => {
         return (
           <CounselorCard
             key={value.wishlistId}
@@ -41,6 +119,8 @@ export const SavedCounselorResults = ({
           />
         );
       })}
+      <div ref={observerElem} />
+      <Space height="3.5rem" />
     </Wrapper>
   );
 };
