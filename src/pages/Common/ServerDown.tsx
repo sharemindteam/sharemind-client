8e55b1e6754aa681b0c1e0553e2a392cf1a31d89
inyoung <rmdnps10@gmail.com>
2024-07-21 16:23:41 +0900

    Style: Complete server shutdown page layout

--- src/pages/Common/ServerDown.tsx
+++ src/pages/Common/ServerDown.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
-
+import { Flex } from 'components/Common/Flex';
+import { Header } from 'components/Common/Header';
+import { ReactComponent as ServerFixIcon } from '../../assets/icons/server-fix.svg';
+import { Heading } from 'styles/font';
+import { Green } from 'styles/color';
 function ServerDown() {
-  return <>ServerDown</>;
+  return (
+    <>
+      <Header isBuyer={false} />
+      <Flex height="calc(100vh - 6rem)" direction="column" gap={'1.5rem'}>
+        <ServerFixIcon />
+        <Heading color={Green}>서비스 점검 중 </Heading>
+      </Flex>
+    </>
+  );
 }
 
 export default ServerDown;
