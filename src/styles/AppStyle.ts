61129a7fcc74001e97e51fc4eb1f1d05e60e85be
kyuhho <rbgh0114@naver.com>
2024-10-10 22:05:47 +0900

    chore: init `APP_HEIGHT` (#380)

--- src/styles/AppStyle.ts
+++ src/styles/AppStyle.ts
@@ -1,3 +1,6 @@
+/** App Height should use under AppLayout (userViewResize) */
+export const APP_HEIGHT = 'calc(var(--vh, 1vh) * 100)';
+
 export const APP_WIDTH = '37.5rem';
 
 export const APP_HEADER_HEIGHT = '5.2rem';
